2d6c2a7146620efcb605a217c90ccbc95dce1476
patevs <ppevans11@gmail.com>
2018-07-23 00:04:58 +1200

    updates

--- scripts/ss_app.groovy
+++ scripts/ss_app.groovy
@@ -6,8 +6,9 @@ node {
 	echoGreen("Starting build job: ${JOB_NAME} on git branch: ${BRANCH}")
 	echoGreen("Build number: ${BUILD_NUMBER}")
 
-	// BUILD VARIABLES
+	// BUILD FOLDERS
 	def PROJECT_DIR = "C:\\xampp-5\\htdocs"
+	def REPORTS_DIR = "${WORKSPACE}\\test-reports"
 
 	// Maven build tool
 	def mvnHome
@@ -16,6 +17,8 @@ node {
 	stage('PREPARE WORKSPACE') { 
 		// clean the job workspace
 		deleteDir()
+		// ensure the reports folder exists
+		bat "mkdir ${REPORTS_DIR}"
 
 		// Get the Maven build tool
 		mvnHome = tool 'M3'
@@ -46,14 +49,25 @@ node {
 		}
 	}
 
+	// DEPENDENCY CHECK STAGE
+	stage('CHECK Dependency Version'){
+		// check for outdated composer dependencies
+		echoGreen("Checking for outdated composer dependencies")
+		// move into project folder
+		dir("${PROJECT_DIR}"){
+			bat "vendor\\bin\\dependensees > nul 2>&1 >> ${REPORTS_DIR}\\dependensees-report.txt"
+			// composer show -i (short for --installed) or composer [global] show -i -t short for --tree
+		}
+	}
+
 	// BUILD STAGE
 	stage('BUILD/REBUILD Database'){
 		// build/rebuild mysql database
-		echoGreen("BUILDING/REBUILDING mysql database")
+		echoGreen("BUILDING/REBUILDING MySQL Database")
 		// move into project folder
 		dir("${PROJECT_DIR}"){
 			bat '''
-				"C:\\Program Files\\Git\\bin\\bash.exe" -c "framework/sake dev/build '' flush=all"
+				:: "C:\\Program Files\\Git\\bin\\bash.exe" -c "framework/sake dev/build '' flush=all"
 				'''
 		}
 	}
@@ -64,13 +78,44 @@ node {
 		echoGreen("RUNNING PHPUnit tests")
 		// move into project folder
 		dir("${PROJECT_DIR}"){
-			//
+			// run php unit tests
+			bat '''
+				:: vendor/bin/phpunit "" siteconfig/tests "" db=mysql flush=all
+				'''
+			bat '''
+				:: "C:\\Program Files\\Git\\bin\\bash.exe" -c "framework/sake dev/tests/all '' flush=all"
+				'''
+			bat '''
+				:: "C:\\Program Files\\Git\\bin\\bash.exe" -c "find **/tests/ -name '*Test.php'"
+				"C:\\Program Files\\Git\\bin\\bash.exe" -c "ls -d **/tests/"
+				'''
+			bat '''
+				:: vendor/bin/fastest --help
+				("C:\\Program Files\\Git\\bin\\bash.exe" -c "ls -d siteconfig/tests/" | vendor\\bin\\fastest "vendor\\bin\\phpunit --testdox-html assets\\phpunit-report\\report.html \"\" {} \"\" db=mysql flush=all") || (exit 0)
+				:: ("C:\\Program Files\\Git\\bin\\bash.exe" -c "ls -d **/tests/" | vendor\\bin\\fastest "vendor\\bin\\phpunit --testdox-html assets\\pphpunit-reports\\report.html \"\" {} \"\" db=mysql flush=all") || (exit 0)
+				'''
+		}
+	}
+
+	// ARCHIVE & PUBLISH STAGE
+	stage('ARCHIVE & PUBLISH Build Reports'){
+		// archiving build reports
+		echoGreen("ARCHIVING build reports")
+		// move into project folder
+		dir("${PROJECT_DIR}"){
+			publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'assets/phpunit-report', reportFiles: 'report.html', reportName: 'PHPUnit Report', reportTitles: ''])
+			publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/dependensees/', reportFiles: 'index.html', reportName: 'Dependencies Report', reportTitles: ''])
+		}
+		// move into reports folder
+		dir("${REPORTS_DIR}"){
+			archiveArtifacts '**'
+			publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: '', reportFiles: 'dependensees-report.txt', reportName: 'Dependencies Summary', reportTitles: ''])
 		}
 	}
 
 } // END OF PIPELINE
-
-// output green text
+ 
+// output green 
 def echoGreen(text){
 	ansiColor('xterm') {
 		echo "\033[32m${text}\033[0m"
